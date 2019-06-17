13e6bf0fbde870c2ef51168a334da5a37564c97c
mimof9 <1509945419@qq.com>
2019-06-17 20:06:53 +0800

    受控组件

--- src/page/HelloWorld.js
+++ src/page/HelloWorld.js
@@ -1,26 +1,91 @@
-import { Card } from 'antd'
+import React, { Component } from 'react'
+import { Card, Tabs, Tree } from 'antd'
 
-export default () => {
-    const style = {
-        width: '400px',
-        margin: '30px',
-        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
-        border: '1px solid #e8e8e8'
+class MyInput extends Component {
+
+    static defaultProps = {
+        value: 18
     }
 
-    return (
-        <Card style={style} actions={[<a>操作一</a>, <a>操作二</a>]}>
-            <Card.Meta
-                avatar={
-                    <img
-                        alt=""
-                        style={{ width: '64px', height: '64px', borderRadius: '32px' }}
-                        src="https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png"
-                    />
-                }
-                title="Alipay"
-                description="在中台产品的研发过程中，会出现不同的设计规范和实现方式，但其中往往存在很多类似的页面和组件，这些类似的组件会被抽离成一套标准规范。"
-            />
-        </Card>
-    )
+    render() {
+        return (
+            <div>
+                <input value={this.props.value} onChange={this.props.onChange}/>
+            </div>
+        )
+    }
+}
+
+export default class HelloWorld extends Component {
+    state = {
+        text: '',
+        activeKey: '2',
+        expandedKeys: [],
+    }
+
+    onTextChange = (e) => {
+        this.setState({
+            text: e.target.value
+        })
+    }
+
+    onTextReset = (e) => {
+        this.setState({
+            text: ''
+        })
+    }
+
+    onTabChange = (activeKey) => {
+        this.setState({ activeKey });
+    }
+
+    onSelect = (selectedKeys) => {
+        const { expandedKeys } = this.state
+        const key = selectedKeys[0]
+
+        if (expandedKeys.includes(key)) {
+            this.setState({
+                expandedKeys: expandedKeys.filter(k => k !== key)
+            })
+        } else {
+            this.setState({
+                expandedKeys: [...expandedKeys, key]
+            })
+        }
+    }
+
+    onExpand = (expandedKeys) => {
+        this.setState({
+            expandedKeys: expandedKeys
+        })
+    }
+
+    render() {
+        const { TabPane } = Tabs
+        const { TreeNode } = Tree
+
+        return (
+            <div>
+                <MyInput value={this.state.text} onChange={this.onTextChange}></MyInput>
+                <button onClick={this.onTextReset}>Reset</button>
+
+                <Tabs activeKey={this.state.activeKey} onChange={this.onTabChange}>
+                    <TabPane tab="Tab 1" key="1">Content of Tab Pane 1</TabPane>
+                    <TabPane tab="Tab 2" key="2">Content of Tab Pane 2</TabPane>
+                </Tabs>
+
+                <Tree
+                    expandedKeys={this.state.expandedKeys}
+                    onExpand={this.onExpand}
+                    selectedKeys={[]}
+                    onSelect={this.onSelect}
+                >
+                    <TreeNode title="parent 1" key="0-0">
+                        <TreeNode title="leaf" key="0-0-0" />
+                        <TreeNode title="leaf" key="0-0-1" />
+                    </TreeNode>
+                </Tree>
+            </div>
+        )
+    }
 }
